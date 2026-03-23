cc6406e715ec2012c000fc8e63710d88c0274f91
Aman Virdi <virdi.aman50@gmail.com>
2026-03-24 00:30:18 +0530

    exploring get, post..

--- src/app.js
+++ src/app.js
@@ -2,8 +2,20 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+    res.send({firstname: "Aman", lastname: "virdi"});
+})
+
+app.post("/user", (req, res) => {
+    res.send("data saved succesfully in database");
+})
+
+app.delete("/user", (req, res) => {
+    res.send("data deleted successfully");
+})
+
 app.use("/test",(req, res) => {
-    res.send("Hello from the server111");
+    res.send("Hello from the server");
 })
 
 app.use("/namaste", (req,res) => {
